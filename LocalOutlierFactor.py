3e038bacbfe932ec0478205f81178429b925bb0e
XENIA <lommy101@gmail.com>
2020-06-24 15:29:43 +0900

    Update LocalOutlierFactor.py

--- LocalOutlierFactor.py
+++ LocalOutlierFactor.py
@@ -1,6 +1,8 @@
-import csv
-import numpy as np
+
 from sklearn.neighbors import LocalOutlierFactor
+import pandas as pd
+import numpy as np
+import csv
 
 def load_data():
     file_name = "" # .csv INPUT
@@ -18,14 +20,12 @@ def load_data():
     print(total_node)
     return total_node
 
-# fit the model for outlier detection (default)
+# fit the model for outlier detection (default : n_neighbors=3, contamination=0.1)
 clf = LocalOutlierFactor(n_neighbors=3, contamination=0.1)
 y_pred = clf.fit_predict(load_data())
 X_scores = clf.negative_outlier_factor_
 X_scores = np.array(X_scores, dtype=np.float64)
 print(-X_scores)
-
-import pandas as pd
 
 df = pd.DataFrame(-X_scores)
 df.to_csv('',index=False) # .csv OUPUT
